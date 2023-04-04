c7b503a1aca21d1457d387268e9b288356b24969
Ilya Tepikin <ilya.tepikin@gmail.com>
2023-04-05 00:19:02 +0300

    Working file transfer

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 // #![windows_subsystem = "windows"]
 
+use std::path::Path;
 use std::{env, thread};
 /*
 GUI Windows-compatible application written in Rust with druid.
 Application has functionality to choose a file and send it over network to another computer.
 */
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::str;
 use std::sync::{Arc, Mutex};
 use tokio::io::{AsyncWriteExt, self, BufReader, AsyncReadExt};
@@ -31,9 +32,9 @@ struct GuiState {
 impl GuiState {
     fn new() -> GuiState {
         GuiState {
-            file_name: "".into(),
-            host: "".into(),
-            port: "".into(),
+            file_name: "".to_string(),
+            host: "192.168.0.105".into(),
+            port: "8080".into(),
             progress: Arc::new(Mutex::from(0.0)),
             incoming_file_name: Arc::new(Mutex::from("".to_string())),
             incoming_file_size: Arc::new(Mutex::from(0)),
@@ -41,11 +42,14 @@ impl GuiState {
     }
 
     fn send_file(&self) {
-        let file_name = self.file_name.clone();
+        let original_file_path = self.file_name.clone();
+        let path = Path::new(self.file_name.as_str());
+        let path_buf = path.to_path_buf();
+        let file_name = path_buf.file_name().unwrap().to_str().unwrap();
         let host = self.host.clone();
         let port = self.port.clone();
 
-        let mut file = File::open(&file_name).unwrap();
+        let mut file = File::open(original_file_path).unwrap();
         let file_size = file.metadata().unwrap().len();
 
         let rt = Builder::new_multi_thread().enable_all().build().unwrap();
@@ -54,7 +58,7 @@ impl GuiState {
                 .await
                 .unwrap();
 
-            let mut file_name_buffer = file_name.into_bytes();
+            let file_name_buffer = file_name.as_bytes();
             let file_name_size = file_name_buffer.len() as u16;
             let file_name_size = file_name_size.to_be_bytes();
             let file_size_buf = file_size.to_be_bytes();
@@ -64,8 +68,9 @@ impl GuiState {
             buf.extend_from_slice(&file_name_size);
             buf.extend_from_slice(&file_size_buf);
             println!("Sending buf: {:?}", buf);
-
             stream.write_all(&buf).await.unwrap();
+
+            stream.write_all(&file_name_buffer).await.unwrap();
 
             let mut buf = [0; 1024];
             let mut total_sent: u64 = 0;
@@ -74,7 +79,13 @@ impl GuiState {
                 if n == 0 {
                     break;
                 }
-                stream.write_all(&buf[0..n]).await.unwrap();
+                match stream.write_all(&buf[0..n]).await {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Error: {}", e);
+                        break;
+                    }
+                }
                 total_sent += n as u64;
                 let percentage = (total_sent as f64 / file_size as f64) * 100.0;
                 *self.progress.lock().unwrap() = percentage;
@@ -152,16 +163,29 @@ async fn process_incoming(socket: TcpStream) -> io::Result<()> {
     let file_size = reader.read_u64().await?;
     println!("file_size: {:?}", file_size);
 
-    // let file_name_size = u16::from_be_bytes(file_name_size);
-    // let file_size = u64::from_be_bytes(file_size);
-
     let mut file_name_buffer = vec![0u8; file_name_size as usize];
     reader.read_exact(&mut file_name_buffer).await?;
     if let Ok(file_name) = String::from_utf8(file_name_buffer) {
         match data_type {
             0x01 => {
                 println!("Received file: {} ({} bytes)", file_name, file_size);
-                // Handle file transfer
+                let mut file = File::create(&file_name).unwrap();
+                let mut total_received: u64 = 0;
+                let mut buf = [0; 1024];
+                while let Ok(n) = reader.read(&mut buf).await {
+                    if n == 0 {
+                        break;
+                    }
+                    if total_received + n as u64 > file_size {
+                        file.write_all(&buf[0..(file_size - total_received) as usize]).unwrap();
+                        break;
+                    } else {
+                        file.write_all(&buf[0..n]).unwrap();
+                        total_received += n as u64;
+                    }
+                    let percentage = (total_received as f64 / file_size as f64) * 100.0;
+                    println!("Received {} bytes ({}%)", total_received, percentage);
+                }
             },
             0x02 => {
                 println!("Received text: {} ({} bytes)", file_name, file_size);
